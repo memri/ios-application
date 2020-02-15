bc627046296ae569041de84ef58c33fc344d4ca1
Ruben Daniels <ruben.daniels@gmail.com>
2020-02-15 13:55:49 +0000

    Update API.swift

--- playground/memriPlayground.playground/Sources/API.swift
+++ playground/memriPlayground.playground/Sources/API.swift
@@ -1,31 +1,34 @@
 /*
  * PodAPI
  */
-PodAPI(String key) // Constructor
-.remove(_ id:String,_ callback: (success:Boolean, error:Error) -> Void)
-.get(_ id:String,_ callback: (item:DataItem, error:Error) -> Void)
-.update(_ id:String,_ item:DataItem,_ callback: (success:Boolean, error:Error) -> Void)
-// Sets the .id property on DataItem
-.create(_ item:DataItem,_ callback: (success:Boolean, error:Error) -> Void)
-.link(_ id:String | item:DataItem, _ id:String | item:DataItem, _ predicate:String, _ callback: (created:Boolean, error:Error) -> Void)
-.unlink(_ id:String | item:DataItem, _ id:String | item:DataItem, _ predicate:String, _ callback: (success:Boolean, error:Error) -> Void)
-.query(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void)
-.queryNLP(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void)
-.queryDSL(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void)
-.queryRAW(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void)
-// Returns a read-only SettingsData object.
-.getDefaultSettings(_ callback: (result:SettingsData, error:Error) -> Void)
-// Returns a read-write SettingsData object.
-.getDeviceSettings(_ callback: (result:SettingsData, error:Error) -> Void)
-// Returns a read-write SettingsData object when admin, otherwise read-only.
-.getGroupSettings(_ groupId:String, _ callback: (result:SettingsData, error:Error) -> Void)
-// Returns a read-write SettingsData object.
-.getUserSettings(_ callback: (result:SettingsData, error:Error) -> Void)
-.import()
-.export()
-.sync()
-.index()
-.convert()
-.augment()
-.automate()
-streamResource(_ URI:String, _ options:StreamOptions, _ callback: (stream:Stream, error:Error) -> Void)
+class PodAPI{
+    func init(String key) {} // Constructor
+
+    func remove(_ id:String,_ callback: (success:Boolean, error:Error) -> Void) {}
+    func get(_ id:String,_ callback: (item:DataItem, error:Error) -> Void) {}
+    func update(_ id:String,_ item:DataItem,_ callback: (success:Boolean, error:Error) -> Void) {}
+    // Sets the .id property on DataItem
+    func create(_ item:DataItem,_ callback: (success:Boolean, error:Error) -> Void) {}
+    func link(_ id:String | item:DataItem, _ id:String | item:DataItem, _ predicate:String, _ callback: (created:Boolean, error:Error) -> Void) {}
+    func unlink(_ id:String | item:DataItem, _ id:String | item:DataItem, _ predicate:String, _ callback: (success:Boolean, error:Error) -> Void) {}
+    func query(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void) {}
+    func queryNLP(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void) {}
+    func queryDSL(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void) {}
+    func queryRAW(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void) {}
+    // Returns a read-only SettingsData object.
+    func getDefaultSettings(_ callback: (result:SettingsData, error:Error) -> Void) {}
+    // Returns a read-write SettingsData object.
+    func getDeviceSettings(_ callback: (result:SettingsData, error:Error) -> Void) {}
+    // Returns a read-write SettingsData object when admin, otherwise read-only.
+    func getGroupSettings(_ groupId:String, _ callback: (result:SettingsData, error:Error) -> Void) {}
+    // Returns a read-write SettingsData object.
+    func getUserSettings(_ callback: (result:SettingsData, error:Error) -> Void) {}
+    func import() {}
+    func export() {}
+    func sync() {}
+    func index() {}
+    func convert() {}
+    func augment() {}
+    func automate() {}
+    func streamResource(_ URI:String, _ options:StreamOptions, _ callback: (stream:Stream, error:Error) -> Void) {}
+}
