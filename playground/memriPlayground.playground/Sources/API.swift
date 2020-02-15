ca61d8485d264c6fbc9b907d5325d77dd575aace
Ruben Daniels <ruben.daniels@gmail.com>
2020-02-15 13:50:39 +0000

    Added beginning of API.swift to specify API more in-depth using the swift syntax

---
+++ playground/memriPlayground.playground/Sources/API.swift
@@ -0,0 +1,35 @@
+/*
+ * PodAPI
+ */
+PodAPI(String key) // Constructor
+.remove(_ id:String,_ callback: (success:Boolean,Error error) -> Void)
+.get(_ id:String,_ callback: (item:DataItem,Error error) -> Void)
+.update(_ id:String,_ item:DataItem,_ callback: (success:Boolean,Error error) -> Void)
+// Sets the .id property on DataItem
+.create(_ item:DataItem,_ callback: (success:Boolean,Error error) -> Void)
+.link(_ id:String | item:DataItem, _ id:String | item:DataItem, _ predicate:String, _ callback: (created:Boolean, error:Error) -> Void)
+.unlink(_ id:String | item:DataItem, _ id:String | item:DataItem, _ predicate:String, _ callback: (success:Boolean, error:Error) -> Void)
+.query(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void)
+.queryNLP(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void)
+.queryDSL(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void)
+.queryRAW(_ query:String,_ options:QueryOptions, _ callback: (result:SearchResult, error:Error) -> Void)
+// Returns a read-only SettingsData object.
+.getDefaultSettings(_ callback: (result:SettingsData, error:Error) -> Void)
+// Returns a read-write SettingsData object.
+.getDeviceSettings(_ callback: (result:SettingsData, error:Error) -> Void)
+// Returns a read-write SettingsData object when admin, otherwise read-only.
+.getGroupSettings(_ groupId:String, _ callback: (result:SettingsData, error:Error) -> Void)
+// Returns a read-write SettingsData object.
+.getUserSettings(_ callback: (result:SettingsData, error:Error) -> Void)
+.import()
+.export()
+.sync()
+.index()
+.convert()
+.augment()
+.automate()
+streamResource(_ URI:String, _ options:StreamOptions, _ callback: (stream:Stream, error:Error) -> Void)
+
+/*
+ * PodAPI
+ */
