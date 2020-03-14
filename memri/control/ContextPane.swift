760b6ef615ed2ecc1ce72a734ad3d393bc43200e
Jess Taylor <wmjesstaylor@gmail.com>
2020-03-13 17:58:55 -0700

    ContextPane, injected current session view, began layout

--- memri/control/ContextPane.swift
+++ memri/control/ContextPane.swift
@@ -8,21 +8,92 @@
 
 import SwiftUI
 
+struct LineDivider: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0.0, y: 0.0))
+        path.addLine(to: CGPoint(x: rect.width, y: 0))
+        return path
+    }
+}
+
+let lineDividerLineWidth: CGFloat = 1.5
+let lineDividerFrameHeight: CGFloat = 5.0
+
 struct ContextPane: View {
     
-    let name = NSLocalizedString("application_name", comment: "")
-    let tag = NSLocalizedString("application_tagline", comment: "")
+    var currentSessionView: SessionView?
+
+    init(sessions: Sessions) {
+        self.currentSessionView = sessions.currentSession.currentSessionView
+    }
+    
+    var title: String?
+    var subtitle: String?
+    var buttons: [ActionDescription] = []
+    var actions: [ActionDescription] = []
+    var navigate: [ActionDescription] = []
+    
+    let actionLabel = NSLocalizedString("actionLabel", comment: "")
+    let navigateLabel = NSLocalizedString("navigateLabel", comment: "")
+    let labelsLabel = NSLocalizedString("labelsLabel", comment: "")
 
     var body: some View {
-        VStack {
-            Text("This is the context pane for \(name)")
-            Text("name = \(name) & tag = \(tag)")
+        VStack (/*alignment: .leading*/){
+            VStack {
+                Text("\(self.currentSessionView?.title ?? "")")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("\(self.currentSessionView?.subtitle ?? "")")
+                    .font(.body)
+                LineDivider()
+                    .stroke(Color.gray, lineWidth: lineDividerLineWidth)
+                    .frame(height: lineDividerFrameHeight)
+            }
+            VStack {
+                Text("some stuff to add later ...")
+                LineDivider()
+                    .stroke(Color.gray, lineWidth: lineDividerLineWidth)
+                    .frame(height: lineDividerFrameHeight)
+            }
+            VStack {
+                HStack {
+                    Text("\(actionLabel)")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                }
+                LineDivider()
+                    .stroke(Color.gray, lineWidth: lineDividerLineWidth)
+                    .frame(height: lineDividerFrameHeight)
+            }
+            VStack {
+                HStack {
+                    Text("\(navigateLabel)")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                }
+                LineDivider()
+                    .stroke(Color.gray, lineWidth: lineDividerLineWidth)
+                    .frame(height: lineDividerFrameHeight)
+            }
+            VStack {
+                HStack {
+                    Text("\(labelsLabel)")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.gray)
+                    Spacer()
+                }
+            }
+            Spacer()
         }
+        .padding()
     }
 }
 
 struct ContentPane_Previews: PreviewProvider {
     static var previews: some View {
-        ContextPane()
+        ContextPane(sessions: try! Sessions.from_json("empty_sessions"))
     }
 }
