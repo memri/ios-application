c911fcdcc2f3512376f7371eb2be18ae976cdd40
Koen van der Veen <koen@Koens-MacBook-Pro.local>
2020-03-20 11:51:15 +0100

    -

---
+++ memri/control/Navigation.swift
@@ -0,0 +1,28 @@
+//
+//  Navigation.swift
+//  memri
+//
+//  Created by Koen van der Veen on 20/03/2020.
+//  Copyright © 2020 memri. All rights reserved.
+//
+
+import SwiftUI
+
+struct Navigation: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Text("11.11").padding(.horizontal)
+                Spacer()
+            }.background(Color.purple)
+
+        
+        }
+    }
+}
+
+struct Navigation_Previews: PreviewProvider {
+    static var previews: some View {
+        Navigation()
+    }
+}
