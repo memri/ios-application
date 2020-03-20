6483d4970c6b86562a979d225fc2af78c32a3ec1
Koen van der Veen <koen@Koens-MacBook-Pro.local>
2020-03-20 18:47:40 +0100

    Navigation UI

--- memri/control/TopNavigation.swift
+++ memri/control/TopNavigation.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct TopNavigation: View {
     @EnvironmentObject var main: Main
-    
+    @State private var showNavigation: Bool = false
     @State private var show_contextpage: Bool = false
     @Binding var isEditMode:EditMode
     
@@ -27,11 +27,16 @@ struct TopNavigation: View {
                 Text(main.currentView.title).font(.headline)
             }
             HStack{
-                Button(action: {}) {
+                Button(action: {
+                    self.showNavigation = true
+                }) {
                     Image(systemName: "line.horizontal.3")
                         .foregroundColor(.gray)
                         .font(Font.system(size: 20, weight: .medium))
-                }.padding(.horizontal, 5)
+                }.sheet(isPresented: self.$showNavigation) {
+                    Navigation()
+                }
+                .padding(.horizontal, 5)
 
                 if main.currentView.backButton != nil {
                     Button(action: backButtonAction ) {
