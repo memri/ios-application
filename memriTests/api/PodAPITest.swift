2646935b9b728248e4e9beecec69c1c25f867c8c
Ruben Daniels <ruben.daniels@gmail.com>
2020-06-15 09:53:30 +0200

    Trying to get the tests to run

--- memriTests/api/PodAPITest.swift
+++ memriTests/api/PodAPITest.swift
@@ -52,7 +52,7 @@ class PodAPITest: XCTestCase {
             XCTAssertNil(error)
             expectation.fulfill()
         }
-        waitForExpectations(timeout: 1, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
     }
     
     func testPodGet() {
@@ -79,7 +79,7 @@ class PodAPITest: XCTestCase {
         
         
         
-        waitForExpectations(timeout: 1, handler: nil)
+        waitForExpectations(timeout: 10, handler: nil)
     }
     
     
