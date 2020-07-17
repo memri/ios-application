bbcb827433066cc374b72a4eea56152f09b3966c
Toby Brennan <contact@apptekstudios.com>
2020-07-17 19:40:33 +1000

    Calendar performance improvements

--- memri/gui/renderers/CalendarRenderer/CalendarRenderer.swift
+++ memri/gui/renderers/CalendarRenderer/CalendarRenderer.swift
@@ -37,6 +37,32 @@ class CascadingCalendarConfig: CascadingRenderConfig {
 	var dateTimeExpression: Expression? { cascadeProperty("dateTime", type: Expression.self) }
 }
 
+struct CalendarCalculations {
+	
+	
+	init(calendarHelper: CalendarHelper, data: [Item], dateResolver: ((Item) -> Date?), renderConfig: CascadingCalendarConfig) {
+		let datesWithItems: [Date: [Item]] = data.reduce(into: [:]) { (result, item) in
+				guard let dateTime = dateResolver(item),
+					let date = calendarHelper.startOfDay(for: dateTime) else { return }
+			result[date] = (result[date] ?? []) + [item]
+			}
+		self.datesWithItems = datesWithItems
+		self.start = datesWithItems.keys.min().flatMap { CalendarHelper().startOfMonth(for: $0) } ?? Date()
+		self.end = datesWithItems.keys.max().flatMap { CalendarHelper().endOfMonth(for: $0) } ?? Date()
+	}
+	var datesWithItems: [Date: [Item]]
+	var start: Date
+	var end: Date
+	
+	func hasItemOnDay(_ day: Date) -> Bool {
+		datesWithItems.keys.contains(day)
+	}
+	
+	func itemsOnDay(_ day: Date) -> [Item] {
+		datesWithItems[day] ?? []
+	}
+}
+
 struct CalendarView: View {
 	@EnvironmentObject var context: MemriContext
 	
@@ -58,80 +84,18 @@ struct CalendarView: View {
 	}
 	
 	@State var scrollPosition: ASCollectionViewScrollPosition? = .bottom
-    
-    
-    var datesWithItems: Set<Date> {
-        data.reduce(into: Set<Date>()) { (result, item) in
-			guard let dateTime = self.resolveExpression(renderConfig.dateTimeExpression, toType: Date.self, forItem: item),
-                  let date = calendarHelper.startOfDay(for: dateTime) else { return }
-            result.insert(date)
-        }
-    }
-    var start: Date {
-        datesWithItems.min().flatMap { CalendarHelper().startOfMonth(for: $0) } ?? Date()
-    }
-    var end: Date {
-        datesWithItems.max().flatMap { CalendarHelper().endOfMonth(for: $0) } ?? Date()
-    }
-    
+
     var calendarHelper = CalendarHelper()
     
-    func hasItemOnDay(_ day: Date) -> Bool {
-        datesWithItems.contains(day)
-    }
-	
-	func itemsOnDay(_ day: Date) -> [Item] {
-		data.filter { item in
-			guard let dateTime = self.resolveExpression(renderConfig.dateTimeExpression, toType: Date.self, forItem: item) else { return false }
-			return calendarHelper.areOnSameDay(day, dateTime)
-		}
-	}
-    
-    func section(forMonth month: Date) -> ASSection<Date> {
-		let days = calendarHelper.getPaddedDays(forMonth: month)
-        return ASSection(id: month, data: days, dataID: \.self) { day, cellContext in
-            Group {
-                day.map { day in
-                    VStack(spacing: 0) {
-                        Spacer()
-						Text(self.calendarHelper.dayString(for: day))
-							.foregroundColor(self.calendarHelper.isToday(day) ? .red : Color(.label))
-						Circle().fill(self.hasItemOnDay(day) ? Color.red : Color.clear)
-                            .frame(width: 10, height: 10)
-                            .padding(4)
-                        Spacer()
-                        Divider()
-                    }
-                    .background(cellContext.isHighlighted ? Color(.secondarySystemBackground) : .clear)
-                }
-            }
-        }
-        .sectionHeader {
-            Text(calendarHelper.monthString(for: month))
-                .font(.headline)
-                .frame(maxWidth: .infinity, alignment: .leading)
-        }
-		.onSelectSingle { index in
-			guard let day = days[safe: index].flatMap({ $0 }) else { return }
-			let formatter = DateFormatter()
-			formatter.dateStyle = .long
-			formatter.timeStyle = .none
-			// handle press on day
-			let items = self.itemsOnDay(day)
-			let uids = items.compactMap { $0.uid }
-			
-			guard let itemType = items.first?.genericType, !uids.isEmpty else { return }
-			
-			try? ActionOpenViewWithUIDs(self.context).exec(["itemType": itemType, "uids": uids])
-		}
-    }
-    
-    var sections: [ASSection<Date>] {
-        calendarHelper.getMonths(from: start, to: end).map(section(forMonth:))
-    }
     
     var body: some View {
-        VStack(spacing: 0) {
+		let calcs = CalendarCalculations(calendarHelper: calendarHelper,
+										 data: context.items,
+										 dateResolver: {
+											self.resolveExpression(renderConfig.dateTimeExpression, forItem: $0)
+		},
+										 renderConfig: renderConfig)
+		return VStack(spacing: 0) {
             HStack(spacing: 0) {
                 ForEach(calendarHelper.daysInWeek, id: \.self) { dayString in
                     Text(dayString)
@@ -141,7 +105,7 @@ struct CalendarView: View {
             }
             .padding(.horizontal, 20)
             .background(Color(.secondarySystemBackground))
-            ASCollectionView(sections: sections)
+            ASCollectionView(sections: sections(withCalcs: calcs))
                 .scrollPositionSetter($scrollPosition)
                 .layout { (layoutEnvironment) -> ASCollectionLayoutSection in
                     .grid(layoutMode: .fixedNumberOfColumns(7), itemSpacing: 0, lineSpacing: 0, itemSize: .absolute(55))
@@ -150,4 +114,49 @@ struct CalendarView: View {
                 .alwaysBounceVertical()
         }
     }
+	
+	
+	func sections(withCalcs calcs: CalendarCalculations) -> [ASSection<Date>] {
+		calendarHelper.getMonths(from: calcs.start, to: calcs.end).map { section(forMonth: $0, withCalcs: calcs) }
+	}
+	
+	
+	func section(forMonth month: Date, withCalcs calcs: CalendarCalculations) -> ASSection<Date> {
+		let days = calendarHelper.getPaddedDays(forMonth: month)
+		return ASSection(id: month, data: days, dataID: \.self) { day, cellContext in
+			Group {
+				day.map { day in
+					VStack(spacing: 0) {
+						Spacer()
+						Text(self.calendarHelper.dayString(for: day))
+							.foregroundColor(self.calendarHelper.isToday(day) ? .red : Color(.label))
+						Circle().fill(calcs.hasItemOnDay(day) ? Color.red : Color.clear)
+							.frame(width: 10, height: 10)
+							.padding(4)
+						Spacer()
+						Divider()
+					}
+					.background(cellContext.isHighlighted ? Color(.secondarySystemBackground) : .clear)
+				}
+			}
+		}
+		.sectionHeader {
+			Text(calendarHelper.monthString(for: month))
+				.font(.headline)
+				.frame(maxWidth: .infinity, alignment: .leading)
+		}
+		.onSelectSingle { index in
+			guard let day = days[safe: index].flatMap({ $0 }) else { return }
+			let formatter = DateFormatter()
+			formatter.dateStyle = .long
+			formatter.timeStyle = .none
+			// handle press on day
+			let items = calcs.itemsOnDay(day)
+			let uids = items.compactMap { $0.uid }
+			
+			guard let itemType = items.first?.genericType, !uids.isEmpty else { return }
+			
+			try? ActionOpenViewWithUIDs(self.context).exec(["itemType": itemType, "uids": uids])
+		}
+	}
 }
