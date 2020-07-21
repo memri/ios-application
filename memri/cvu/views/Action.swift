ec6f4a9b1c7173f02119317a6a8795d0fa140f11
Ruben Daniels <ruben.daniels@gmail.com>
2020-07-21 23:05:53 +0200

    more cleanup

--- memri/cvu/views/Action.swift
+++ memri/cvu/views/Action.swift
@@ -335,7 +335,7 @@ public enum RenderType: String {
 }
 
 public enum ActionFamily: String, CaseIterable {
-	case back, addItem, openView, openDynamicView, openViewByName, openGroup, openViewByChangingRenderer, toggleEditMode, toggleFilterPanel,
+	case back, addItem, openView, openDynamicView, openViewByName, openGroup, toggleEditMode, toggleFilterPanel,
 		star, showStarred, showContextPane, showOverlay, share, showNavigation, addToPanel, duplicate,
 		schedule, addToList, duplicateNote, noteTimeline, starredNotes, allNotes, exampleUnpack,
 		delete, setRenderer, select, selectAll, unselectAll, showAddLabel, openLabelView,
@@ -349,7 +349,6 @@ public enum ActionFamily: String, CaseIterable {
 		case .addItem: return ActionAddItem.self
 		case .openView: return ActionOpenView.self
 		case .openGroup: return ActionOpenViewWithUIDs.self
-		case .openViewByChangingRenderer: return ActionNewViewByChangingRenderer.self
 		case .openViewByName: return ActionOpenViewByName.self
 		case .toggleEditMode: return ActionToggleEditMode.self
 		case .toggleFilterPanel: return ActionToggleFilterPanel.self
