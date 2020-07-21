85a2a8e96039fe0e435c9c5c6ee71e17fa535921
Toby Brennan <contact@apptekstudios.com>
2020-07-21 19:18:20 +1000

    Bump build number (required to be unique for testflight) Fix realm merge regression - document reason for optional Fix edit button merge regression Remove unused buildConfig (RUNBUILDTOOLS).

--- memri/model/DatabaseController.swift
+++ memri/model/DatabaseController.swift
@@ -50,6 +50,7 @@ class DatabaseController {
 	
 	private static var realmConfig: Realm.Configuration {
 		Realm.Configuration(
+			// Set the file url (this is set to nil on actual devices -> uses the default directory)
 			fileURL: try! getRealmURL(),
 			// Set the new schema version. This must be greater than the previously used
 			// version (if you've never set a schema version before, the version is 0).
@@ -69,8 +70,8 @@ class DatabaseController {
 	}
 	
 	/// Computes the Realm database path at /home/<user>/realm.memri/memri.realm and creates the directory (realm.memri) if it does not exist.
-	/// - Returns: the computed database file path
-	static func getRealmURL() throws -> URL {
+	/// - Returns: the computed database file url, or NIL if it should use the default.
+	static func getRealmURL() throws -> URL? {
 		#if targetEnvironment(simulator)
 		if let homeDir = ProcessInfo.processInfo.environment["SIMULATOR_HOST_HOME"] {
 			var realmDir = homeDir + "/realm.memri"
