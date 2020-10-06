979b6e2ff3d987f8ad030c0461958d57807914dd
rubenseggers <rubenseggers@gmail.com>
2020-10-06 10:25:55 +0200

    schema update release

--- memri/model/schema/schema-autogen.swift
+++ memri/model/schema/schema-autogen.swift
@@ -27,9 +27,11 @@ enum ItemFamily: String, ClassFamily, CaseIterable {
     case typeCountry = "Country"
     case typeCreativeWork = "CreativeWork"
     case typeCryptoKey = "CryptoKey"
+    case typeDatasource = "Datasource"
     case typeDevice = "Device"
     case typeDiet = "Diet"
     case typeDownloader = "Downloader"
+    case typeEdge = "Edge"
     case typeEmailMessage = "EmailMessage"
     case typeEvent = "Event"
     case typeExercisePlan = "ExercisePlan"
@@ -84,15 +86,14 @@ enum ItemFamily: String, ClassFamily, CaseIterable {
     case typeTransaction = "Transaction"
     case typeTrip = "Trip"
     case typeUnit = "Unit"
+    case typeUserState = "UserState"
     case typeVideo = "Video"
+    case typeViewArguments = "ViewArguments"
     case typeVisualArt = "VisualArt"
     case typeVote = "Vote"
     case typeVoteAction = "VoteAction"
     case typeWebsite = "Website"
     case typeWrittenWork = "WrittenWork"
-    case typeLabelAnnotation = "LabelAnnotation"
-    case typeLabelAnnotationCollection = "LabelAnnotationCollection"
-    case typePhotoAnnotation = "PhotoAnnotation"
 
     static var discriminator: Discriminator = ._type
 
@@ -103,13 +104,17 @@ enum ItemFamily: String, ClassFamily, CaseIterable {
         case .typeArticle: return Color(hex: "#93c47d")
         case .typeAudio: return Color(hex: "#93c47d")
         case .typeAuditItem: return Color(hex: "#93c47d")
+        case .typeCVUStateDefinition: return Color(hex: "#93c47d")
+        case .typeCVUStoredDefinition: return Color(hex: "#93c47d")
         case .typeComment: return Color(hex: "#93c47d")
         case .typeCountry: return Color(hex: "#93c47d")
         case .typeCreativeWork: return Color(hex: "#93c47d")
         case .typeCryptoKey: return Color(hex: "#93c47d")
+        case .typeDatasource: return Color(hex: "#93c47d")
         case .typeDevice: return Color(hex: "#93c47d")
         case .typeDiet: return Color(hex: "#37af1c")
         case .typeDownloader: return Color(hex: "#93c47d")
+        case .typeEdge: return Color(hex: "#93c47d")
         case .typeEmailMessage: return Color(hex: "#93c47d")
         case .typeEvent: return Color(hex: "#93c47d")
         case .typeExercisePlan: return Color(hex: "#93c47d")
@@ -164,7 +169,9 @@ enum ItemFamily: String, ClassFamily, CaseIterable {
         case .typeTransaction: return Color(hex: "#3a5eb2")
         case .typeTrip: return Color(hex: "#93c47d")
         case .typeUnit: return Color(hex: "#93c47d")
+        case .typeUserState: return Color(hex: "#93c47d")
         case .typeVideo: return Color(hex: "#93c47d")
+        case .typeViewArguments: return Color(hex: "#93c47d")
         case .typeVisualArt: return Color(hex: "#93c47d")
         case .typeVote: return Color(hex: "#93c47d")
         case .typeVoteAction: return Color(hex: "#93c47d")
@@ -181,13 +188,17 @@ enum ItemFamily: String, ClassFamily, CaseIterable {
         case .typeArticle: return Color(hex: "#ffffff")
         case .typeAudio: return Color(hex: "#ffffff")
         case .typeAuditItem: return Color(hex: "#ffffff")
+        case .typeCVUStateDefinition: return Color(hex: "#ffffff")
+        case .typeCVUStoredDefinition: return Color(hex: "#ffffff")
         case .typeComment: return Color(hex: "#ffffff")
         case .typeCountry: return Color(hex: "#ffffff")
         case .typeCreativeWork: return Color(hex: "#ffffff")
         case .typeCryptoKey: return Color(hex: "#ffffff")
+        case .typeDatasource: return Color(hex: "#ffffff")
         case .typeDevice: return Color(hex: "#ffffff")
         case .typeDiet: return Color(hex: "#ffffff")
         case .typeDownloader: return Color(hex: "#ffffff")
+        case .typeEdge: return Color(hex: "#ffffff")
         case .typeEmailMessage: return Color(hex: "#ffffff")
         case .typeEvent: return Color(hex: "#ffffff")
         case .typeExercisePlan: return Color(hex: "#ffffff")
@@ -242,7 +253,9 @@ enum ItemFamily: String, ClassFamily, CaseIterable {
         case .typeTransaction: return Color(hex: "#ffffff")
         case .typeTrip: return Color(hex: "#ffffff")
         case .typeUnit: return Color(hex: "#ffffff")
+        case .typeUserState: return Color(hex: "#ffffff")
         case .typeVideo: return Color(hex: "#ffffff")
+        case .typeViewArguments: return Color(hex: "#ffffff")
         case .typeVisualArt: return Color(hex: "#ffffff")
         case .typeVote: return Color(hex: "#ffffff")
         case .typeVoteAction: return Color(hex: "#ffffff")
@@ -269,9 +282,11 @@ enum ItemFamily: String, ClassFamily, CaseIterable {
         case .typeCountry: return Country.self
         case .typeCreativeWork: return CreativeWork.self
         case .typeCryptoKey: return CryptoKey.self
+        case .typeDatasource: return Datasource.self
         case .typeDevice: return Device.self
         case .typeDiet: return Diet.self
         case .typeDownloader: return Downloader.self
+        case .typeEdge: return Edge.self
         case .typeEmailMessage: return EmailMessage.self
         case .typeEvent: return Event.self
         case .typeExercisePlan: return ExercisePlan.self
@@ -326,15 +341,14 @@ enum ItemFamily: String, ClassFamily, CaseIterable {
         case .typeTransaction: return Transaction.self
         case .typeTrip: return Trip.self
         case .typeUnit: return Unit.self
+        case .typeUserState: return UserState.self
         case .typeVideo: return Video.self
+        case .typeViewArguments: return ViewArguments.self
         case .typeVisualArt: return VisualArt.self
         case .typeVote: return Vote.self
         case .typeVoteAction: return VoteAction.self
         case .typeWebsite: return Website.self
         case .typeWrittenWork: return WrittenWork.self
-        case .typeLabelAnnotation: return LabelAnnotation.self
-        case .typeLabelAnnotationCollection: return LabelAnnotationCollection.self
-        case .typePhotoAnnotation: return PhotoAnnotation.self
         }
     }
 }
@@ -1363,6 +1377,41 @@ public class ExercisePlan : Item {
     }
 }
 
+/// Any file that can be stored on disk.
+public class File : Item {
+    /// The sha256 hash of a resource.
+    @objc dynamic var sha256:String? = nil
+    /// A cryptographic nonce https://en.wikipedia.org/wiki/Cryptographic_nonce
+    @objc dynamic var nonce:String? = nil
+    /// A piece of information that determines the functional output of a cryptographic
+    /// algorithm.
+    @objc dynamic var key:String? = nil
+    /// The filename of a resource.
+    @objc dynamic var filename:String? = nil
+
+    /// A universal resource location
+    var resource: Results<Resource>? {
+        edges("resource")?.items(type:Resource.self)
+    }
+
+    /// An Item this Item is used by.
+    var usedBy: [Item]? {
+        edges("usedBy")?.itemsArray()
+    }
+
+    public required convenience init(from decoder: Decoder) throws {
+        self.init()
+
+        jsonErrorHandling(decoder) {
+            sha256 = try decoder.decodeIfPresent("sha256") ?? sha256
+            nonce = try decoder.decodeIfPresent("nonce") ?? nonce
+            key = try decoder.decodeIfPresent("key") ?? key
+            filename = try decoder.decodeIfPresent("filename") ?? filename
+
+            try self.superDecode(from: decoder)
+        }
+    }
+}
 
 /// The number of occurrences of a repeating event per measure of time.
 public class Frequency : Item {
@@ -1626,7 +1675,7 @@ public class ImporterRun : Item {
     @objc dynamic var repository:String? = nil
     /// The progress an Item made. The number could be a (rounded) percentage or a count of a
     /// (potentially unknown) total.
-    let progress = RealmOptional<Int>()
+    let progress = RealmOptional<Double>()
     /// The type of the data this Item acts on.
     @objc dynamic var dataType:String? = nil
     /// Username of an importer.
@@ -1716,7 +1765,7 @@ public class IndexerRun : Item {
     @objc dynamic var query:String? = nil
     /// The progress an Item made. The number could be a (rounded) percentage or a count of a
     /// (potentially unknown) total.
-    let progress = RealmOptional<Int>()
+    let progress = RealmOptional<Double>()
     /// The type of data this Item targets.
     @objc dynamic var targetDataType:String? = nil
     /// The status of a run, (running, error, etc).
